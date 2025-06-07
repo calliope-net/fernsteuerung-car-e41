2ddc63109291606159659c2fff423f5ffd0ef4f6
calliope-net <141074597+calliope-net@users.noreply.github.com>
2025-06-07 17:58:04 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -13,6 +13,9 @@ function zeigeStatus2 () {
 pins.onPulsed(DigitalPin.C16, PulseValue.Low, function () {
 	
 })
+btf.onReceivedDataChanged(function (receivedData, changed) {
+    receiver.selectMotor128Servo16(btf.getByte(receivedData, btf.eBufferPointer.m0, btf.eBufferOffset.b0_Motor), btf.getByte(receivedData, btf.eBufferPointer.m0, btf.eBufferOffset.b1_Servo))
+})
 receiver.beimStart(
 receiver.eHardware.car4,
 90,
@@ -20,6 +23,13 @@ false,
 65,
 true
 )
+for (let Index = 0; Index <= 9; Index++) {
+    if (!(receiver.qwiicMotorStatus(receiver.eQwiicMotorChip.ab))) {
+        basic.showNumber(Index)
+        basic.pause(2000)
+    }
+}
+receiver.qwiicMotorChipPower(receiver.eQwiicMotorChip.ab, true)
 loops.everyInterval(1000, function () {
 	
 })
