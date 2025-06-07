690845c95a66c1aab523a9541043110bc59d9fee
calliope-net <141074597+calliope-net@users.noreply.github.com>
2025-06-07 14:44:02 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -13,6 +13,13 @@ function zeigeStatus2 () {
 pins.onPulsed(DigitalPin.C16, PulseValue.Low, function () {
 	
 })
+receiver.beimStart(
+receiver.eHardware.car4,
+90,
+false,
+65,
+true
+)
 loops.everyInterval(1000, function () {
 	
 })
