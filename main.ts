6daddc1c60e9969c206cbf6b380853d0ab19790f
calliope-net <141074597+calliope-net@users.noreply.github.com>
2025-06-07 18:11:16 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,8 @@
-car4.onReceivedData(function (receivedBuffer) {
-	
-})
 function zeigeStatus () {
 	
 }
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
-	
+    receiver.pinRelay(false)
 })
 function zeigeStatus2 () {
 	
@@ -14,22 +11,25 @@ pins.onPulsed(DigitalPin.C16, PulseValue.Low, function () {
 	
 })
 btf.onReceivedDataChanged(function (receivedData, changed) {
+    receiver.qwiicMotorChipPower(receiver.eQwiicMotorChip.ab, btf.getaktiviert(receivedData, btf.e3aktiviert.m0))
     receiver.selectMotor128Servo16(btf.getByte(receivedData, btf.eBufferPointer.m0, btf.eBufferOffset.b0_Motor), btf.getByte(receivedData, btf.eBufferPointer.m0, btf.eBufferOffset.b1_Servo))
 })
-receiver.beimStart(
-receiver.eHardware.car4,
-90,
-false,
-65,
-true
-)
-for (let Index = 0; Index <= 9; Index++) {
-    if (!(receiver.qwiicMotorStatus(receiver.eQwiicMotorChip.ab))) {
-        basic.showNumber(Index)
-        basic.pause(2000)
+if (!(btf.simulator())) {
+    led.enable(false)
+    receiver.beimStart(
+    receiver.eHardware.car4,
+    90,
+    false,
+    65,
+    true
+    )
+    for (let Index = 0; Index <= 9; Index++) {
+        if (!(receiver.qwiicMotorStatus(receiver.eQwiicMotorChip.ab))) {
+            basic.showNumber(Index)
+            basic.pause(2000)
+        }
     }
 }
-receiver.qwiicMotorChipPower(receiver.eQwiicMotorChip.ab, true)
 loops.everyInterval(1000, function () {
 	
 })
