3a8b304a94fb63c59432adcebabbf44fbc8ab11e
calliope-net <141074597+calliope-net@users.noreply.github.com>
2025-06-08 08:39:54 +0200

    Aktualisiere pxt.json, car4.ts, aktoren.ts

---
+++ aktoren.ts
@@ -0,0 +1,50 @@
+
+namespace car {
+
+    const pinBuzzer = DigitalPin.P3         // 5V Grove Buzzer
+    const pinLicht = DigitalPin.C7          // 5V Licht
+
+    let n_Licht = false
+    let n_buzzer = false
+
+
+    //% group="Hupe" subcategory="Aktoren"
+    //% block="Hupe %pON"
+    //% pON.shadow="toggleOnOff"
+    export function buzzer(pON: boolean) {
+        if (n_buzzer !== pON) {
+            n_buzzer = pON
+            pins.digitalWritePin(pinBuzzer, n_buzzer ? 1 : 0)
+        }
+    }
+
+
+
+    //% group="Licht" subcategory="Aktoren"
+    //% block="Licht %pON || blinken %pBlink" weight=6
+    //% pON.shadow="toggleOnOff" pBlink.shadow="toggleOnOff"
+    export function licht(pON: boolean, pBlink = false) {
+        if (pON && pBlink)
+            n_Licht = !n_Licht
+        else
+            n_Licht = pON
+        pins.digitalWritePin(pinLicht, n_Licht ? 0 : 1) // an bei digitalem Wert 0
+    }
+
+    //% group="Licht" subcategory="Aktoren"
+    //% block="Licht an ?" weight=4
+    export function licht_get() { return n_Licht }
+
+    //% group="Licht" subcategory="Aktoren"
+    //% block="Licht aus < %aus an > %an bei Helligkeit" weight=2
+    //% aus.defl=200 an.defl=300
+    export function licht_sensor(aus: number, an: number) {
+        if (n_Licht && helligkeit_analog() < aus) {
+            licht(false)
+        } else if (!n_Licht && helligkeit_analog() > an) {
+            licht(true)
+        }
+    }
+
+
+}
